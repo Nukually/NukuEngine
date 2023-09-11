3e5a745a8480fa55938bfc248b02f52d5f572385
Nukually <317255300@qq.com>
2023-09-11 21:04:36 +0800

    feat:首次提交

---
+++ .env.cmake
@@ -0,0 +1,9 @@
+set(GLFW_PATH C:/glfw-3.3.8.bin.WIN64)
+set(GLM_PATH C:/glm)
+set(VULKAN_SDK_PATH  C:/VulkanSDK/1.3.216.0)
+
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+# set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
+
+# Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
+# set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
